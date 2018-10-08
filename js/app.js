d98190b51d215750c13816a54d76ffa2538e52f9
LCifuentes2018 <43303054+LCifuentes2018@users.noreply.github.com>
2018-10-07 21:15:49 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,243 +1,122 @@
-/* Fields */
-const nonMatchingCardTime = 500;
-let countMoves = 0;
-let indexOpenCard = -1;
-let cardObjectList = [];
-let modal=null;
-
-/**
-* @description Create Card Object
-* @param {index} The index of card
-* @param {value} The value of css
-* @returns {cardObj} Card Object
-*/
-function Card(index, value) {
-	let cardObj = new Object();
-	cardObj.index = index
-	cardObj.value = value
-	cardObj.isRevealed = false
-	cardObj.isMatchingCard = false
-	cardObj.matchingCard = function() {
-		cardObj.isMatchingCard = true
-	}
-	cardObj.reveal = function() {
-		cardObj.isRevealed = true
-	}
-	cardObj.conceal = function() {
-		cardObj.isRevealed = false
-	}
-	return cardObj
-};
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-	var currentIndex = array.length,
-		temporaryValue, randomIndex
-	while (currentIndex !== 0) {
-		randomIndex = Math.floor(Math.random() * currentIndex)
-		currentIndex -= 1
-		temporaryValue = array[currentIndex]
-		array[currentIndex] = array[randomIndex]
-		array[randomIndex] = temporaryValue
-	}
-	return array
-};
-
-function exitsCardOpen(currentValue, index) {
-	if (currentValue.isRevealed === true) {
-		indexOpenCard = index;
-		return true;
-	}
+//Fields
+let winMoves = 0
+let crashMoves = 0
+// Enemies our player must avoid
+var Enemy = function (xMove, yMove, speedEnemy) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = xMove
+  this.y = yMove
+  this.speed = speedEnemy
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png'
 }
 
-function machtedCount(value) {
-	return value.isMatchingCard === true;
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt
+
+  // The enemy outside the boundaries
+  if (this.x > 505) {
+    this.x = 0
+    this.speed = 120 + Math.floor(Math.random() * 620)
+  }
+
+  // Crash between enemy and player
+  if (playerPink.x < this.x + 30 &&
+    playerPink.x + 30 > this.x &&
+    playerPink.y < this.y + 30 &&
+    30 + playerPink.y > this.y) {
+    crashMoves = crashMoves + 1
+    let mod = crashMoves % 3
+    if (mod === 0 && winMoves != 0) {
+      winMoves = winMoves - 1
+      document.getElementById('score').innerText = winMoves
+    }
+    playerPink.y = 300
+    playerPink.x = 210
+  } 
 }
 
-function remove(el) {
-	el.classList.remove('open', 'show', 'wrong')
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
 }
 
-function hideCard() {
-	const childNodes = document.getElementsByTagName('li');
-	for (let i = 0; i < childNodes.length; i++) {
-		let card = childNodes[i];
-		if (childNodes[i].classList.contains('open', 'show')) {
-			childNodes[i].classList.add('wrong');
-			if (childNodes[i].classList.contains('wrong')) {
-				setTimeout(function() {
-					remove(card);
-				}, 1200);
-			}
-		}
-	}
+// Now write your own player class
+let OwnPlayer = function (xPlayer, yPlayer, speedPlayer) {
+  this.x = xPlayer
+  this.y = yPlayer
+  this.speed = speedPlayer
+  this.sprite = 'images/char-boy.png'
+}
+// This class requires an update(), render() and
+// a handleInput() method.
+OwnPlayer.prototype.update = function () {
+  // The player outside the boundaries
+  if (this.x > 400) {
+    this.x = 400
+  }
+  if (this.y > 440) {
+    this.y = 440
+  }
+  if (this.x < 0) {
+    this.x = 0
+  }
+  if (this.y < 0) {
+    winMoves = winMoves + 1
+    document.getElementById('score').innerText = winMoves
+    this.y = 300
+    this.x = 210
+  }
 }
 
-function matchCard() {
-	const childNodes = document.getElementsByTagName('li');
-	for (let i = 0; i < childNodes.length; i++) {
-		if (childNodes[i].classList.contains('open', 'show')) {
-			childNodes[i].classList.remove('open', 'show');
-			childNodes[i].classList.add('match');
-		}
-	}
+OwnPlayer.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
 }
 
-function gameScore() {
-	countMoves = countMoves + 1;
-	document.getElementById('moves').innerText = countMoves;
-	if (countMoves === 8 || countMoves === 14 || countMoves === 20) {
-		let starts = document.getElementsByClassName('fa', 'fa-star');
-		starts[0].classList.remove('fa', 'fa-star');
-	}
+OwnPlayer.prototype.handleInput = function (key) {
+  switch (key) {
+    case 'left':
+      this.x -= this.speed + 30
+      break
+    case 'up':
+      this.y -= this.speed + 60
+      break
+    case 'right':
+      this.x += this.speed + 30
+      break
+    case 'down':
+      this.y += this.speed + 60
+      break
+  }
 }
+// Now instantiate your objects.
+let allEnemies = []
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+const playerPink = new OwnPlayer(210, 320, 20)
+var enemyPos = [60, 140, 230]
+let enemy
+enemyPos.forEach(function (y) {
+  enemy = new Enemy(0, y, 120 + Math.floor(Math.random() * 620))
+  allEnemies.push(enemy)
+})
 
-/**
-* @description Handle clicking on card
-* @param {event} The click event
-*/
-let handleFlipCard = function(event) {
-	event.preventDefault();
-	let cardObject = cardObjectList[this.index - 3];
-	if (cardObject.isMatchingCard === true) {
-		return
-	}
-	cardObjectList.forEach(exitsCardOpen);
-	const cardsEle = document.getElementById('memorycards');
-	let openCard = cardsEle.getElementsByClassName('open', 'show');
-	if (openCard.length < 2) {
-		let cardCurrent = document.getElementsByTagName('li')[this.index]
-		cardCurrent.classList.add('open', 'show')
-		cardObject.reveal();
-		if (indexOpenCard !== -1 && openCard.length === 2) {
-			let previousOpen = cardObjectList[indexOpenCard]
-			if (previousOpen.value === cardObject.value) {
-				matchCard()
-				previousOpen.matchingCard();
-				cardObject.matchingCard();
-				previousOpen.conceal();
-				cardObject.conceal();
-			} else {
-				setTimeout(function() {
-					hideCard();
-					previousOpen.conceal();
-					cardObject.conceal();
-				}.bind(status), nonMatchingCardTime);
-			}
-			indexOpenCard = -1;
-			/* Game Score*/
-			gameScore();
-			/* Validate memory game winning*/
-			const matchCards = cardObjectList.filter(machtedCount);
-			if (matchCards.length === 16) {
-				startClock();
-				const span = document.getElementsByClassName("close")[0];
-				const btn = document.getElementById("btnAgain");
-				const starlist =document.getElementById("stars"); 
-				const stars = starlist.getElementsByClassName('fa-star').length;
-				document.getElementById("movesCount").innerText = countMoves;
-				document.getElementById("starsCount").innerText = stars;
-				document.getElementById("elapsedTime").innerText = document.getElementById('crono').innerHTML;
-				modal = document.getElementById('myModal');
-				modal.style.display = "block";
-				span.onclick = function() {
-					modal.style.display = "none";
-				}
-				btn.onclick = function() {
-					resetGame();
-					modal.style.display = "none";
-				}
-			}
-		}
-	}
-};
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function (e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  }
 
-// Build single card
-const buildCardNode = function(index, card) {
-	let flipContainer = document.createElement('li')
-	let flipper = document.createElement('i')
-	flipContainer.index = index + 3
-	flipContainer.classList.add('card') 
-	flipper.classList.add('fa', card.value)
-	flipContainer.appendChild(flipper)
-	flipContainer.addEventListener('click', handleFlipCard)
-	return flipContainer
-};
-// Build cards list
-const createCards = function() {
-	const cardList = ['fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb']
-	let cards = []
-	let count = 0
-	const maxValue = 8
-	while (count < maxValue) {
-		cards[2 * count] = new Card(count + 1, cardList[count])
-		cards[2 * count + 1] = new Card(count + 1, cardList[count])
-		count++
-	}
-	cardObjectList = shuffle(cards);
-	return cardObjectList
-};
-
-function buildCardNodes() {
-	const memoryCards = document.getElementById('memorycards');
-	const cards = createCards();
-	for (let i = 0; i < 16; i++) {
-		const newLiElement = buildCardNode(i, cards[i]);
-		memoryCards.appendChild(newLiElement);
-	}
-	startClock();
-}
-/* Reset Game */
-function resetGame() {
-	timeout = 0;
-	indexOpenCard = -1;
-	countMoves = 0;
-	let cards = document.getElementById("memorycards");
-	while (cards.hasChildNodes()) {
-		cards.removeChild(cards.firstChild);
-	}
-	const stars = document.getElementsByTagName('i');
-	for (let i = 0; i < stars.length; i++) {
-		stars[i].classList.toggle('fa', true);
-		stars[i].classList.toggle('fa-star', true);
-	}
-	document.getElementById('moves').innerText = countMoves;
-	if (cardObjectList.length > 0) {
-		cardObjectList = [];
-	}
-	buildCardNodes();
-}
-/* Cronometer*/
-let init = 0;
-let timeout = 0;
-
-function startClock() {
-	if (timeout == 0) {
-		init = new Date().getTime();
-		working();
-	} else {
-		clearTimeout(timeout);
-		timeout = 0;
-	}
-}
-
-function working() {
-	const current = new Date().getTime();
-	let diff = new Date(current - init);
-	let result = LeadingZero(diff.getUTCHours()) + ":" + LeadingZero(diff.getUTCMinutes()) + ":" + LeadingZero(diff.getUTCSeconds());
-	document.getElementById('crono').innerHTML = result;
-	timeout = setTimeout("working()", 1000);
-}
-
-function LeadingZero(Time) {
-	return (Time < 10) ? "0" + Time : +Time;
-}
-window.addEventListener("load", buildCardNodes)
-document.getElementById("reset").onclick = resetGame;
-window.onclick = function(event) {
-	if (modal !== null) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	 }
-}
+  playerPink.handleInput(allowedKeys[e.keyCode])
+})
